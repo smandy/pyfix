5bf708ec7e7ac9cbd37fa2649e576b2d9536e64a
Andy Smith <andyrsmith@gmail.com>
2023-09-18 14:19:10 +0100

    Flake8

--- pyfix/FIXPerspective.py
+++ pyfix/FIXPerspective.py
@@ -2,6 +2,7 @@ from twisted.spread import pb
 from twisted.internet import reactor, task
 from pyfix.alladin.Discovery import Discoverable
 
+
 class FIXPerspective(pb.Root):
     def __init__(self, session_manager, description="FIX Perspective"):
         self.session_listeners = []
@@ -13,43 +14,49 @@ class FIXPerspective(pb.Root):
         self.description = description
         data = {'description': self.description,
                 'port': port}
-        d = Discoverable(data, domain="pyfix_perspective")
-        l = task.LoopingCall(self.update_listeners)
-        reactor.callWhenRunning(l.start, 1)
+        l2 = Discoverable(data, domain="pyfix_perspective")
+        task.LoopingCall(self.update_listeners)
+        reactor.callWhenRunning(l2.start, 1)
 
     def remote_getSessionStatus(self):
         ret = []
         for session in self.session_manager.sessions:
-            ret.append(
-                (session.sender, session.target, session.isConnected(), session.inMsgSeqNum, session.outMsgSeqNum))
+            ret.append((session.sender, session.target,
+                        session.isConnected(),
+                        session.inMsgSeqNum, session.outMsgSeqNum))
         return ret
 
     getSessionStatus = remote_getSessionStatus
 
     def remote_addSessionListener(self, listener):
-        print "AddSessionListener %s" % listener
-        listener.notifyOnDisconnect(lambda x: self.expunge(self.session_listeners, listener))
+        print(f"AddSessionListener {listener}")
+        listener.notifyOnDisconnect(lambda x: self.expunge(
+            self.session_listeners, listener))
         self.session_listeners.append(listener)
 
-    def on_execution(self, source, execution):
+    def on_execution(self, _, execution):
         to_iter = self.session_listeners[:]
         for x in to_iter:
-            x.callRemote('onExecution', execution.to_fix()).addErrback(self.expunge, self.session_listeners, x)
+            x.callRemote('onExecution', execution.to_fix()).addErrback(
+                self.expunge, self.session_listeners, x)
 
-    def on_order(self, source, order):
+    def on_order(self, _, order):
         to_iter = self.session_listeners[:]
         for x in to_iter:
-            x.callRemote('onOrder', order.to_fix()).addErrback(self.expunge, self.session_listeners, x)
+            x.callRemote('onOrder', order.to_fix()).addErrback(
+                self.expunge, self.session_listeners, x)
 
     def update_listeners(self):
-        print "UpdateListeners ( of which there are %s)" % len(self.session_listeners)
+        print(f"UpdateListeners ( of which there are "
+              f"{len(self.session_listeners)} )")
         ss = self.getSessionStatus()
         to_iter = self.session_listeners[:]
         for x in to_iter:
-            x.callRemote('onSessionState', ss).addErrback(self.expunge, self.session_listeners, x)
+            x.callRemote('onSessionState', ss).addErrback(
+                self.expunge, self.session_listeners, x)
 
     @staticmethod
-    def expunge(l, victim):
-        print "Expunging %s" % victim
-        while victim in l:
-            l.remove(victim)
+    def expunge(xs, victim):
+        print(f"Expunging {victim}")
+        while victim in xs:
+            xs.remove(victim)
