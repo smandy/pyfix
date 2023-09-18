fea3a7e82aa8d88dce95daf6bc4d6dfb5c516529
Andy Smith <andyrsmith@gmail.com>
2023-09-18 13:46:07 +0100

    Flake8

--- messages/Session.py
+++ messages/Session.py
@@ -1,42 +1,40 @@
-
 from datetime import datetime
 from makeSpec import fix
+from twisted.internet.protocol import ClientFactory
+
 
 class DummySession:
     def __init__(self,
-                 beginString, 
+                 beginString,
                  senderCompID,
-                 targetCompID ):
+                 targetCompID):
 
         self.sender = senderCompID
         self.target = targetCompID
-        
-        self.beginString  = fix.BeginString( beginString )
-        self.senderCompID = fix.SenderCompID( senderCompID )
-        self.targetCompID = fix.TargetCompID( targetCompID )
+
+        self.beginString = fix.BeginString(beginString)
+        self.senderCompID = fix.SenderCompID(senderCompID)
+        self.targetCompID = fix.TargetCompID(targetCompID)
         self.outMsgSeqNum = 1
-        self.inMsgSeqNum  = 1
-        
-        #self.bodyLength = pyfix.BodyLength(0)
-        #self.checkSum   = pyfix.CheckSum( 0 )
+        self.inMsgSeqNum = 1
 
     def setPersister(self, persister):
         self.persister = persister
 
     def compileMessage(self, msg):
-        sendingTime = fix.SendingTime( datetime.now() )
+        sendingTime = fix.SendingTime(datetime.now())
         bodyLength = fix.BodyLength(0)
-        checkSum   = fix.CheckSum(0)
+        checkSum = fix.CheckSum(0)
 
-        header = [ self.beginString,
-                   bodyLength,
-                   msg.msgTypeField,
-                   self.senderCompID,
-                   self.targetCompID,
-                   fix.MsgSeqNum(self.msgSeqNum),
-                   sendingTime]
+        header = [self.beginString,
+                  bodyLength,
+                  msg.msgTypeField,
+                  self.senderCompID,
+                  self.targetCompID,
+                  fix.MsgSeqNum(self.msgSeqNum),
+                  sendingTime]
 
-        footer = [ checkSum ]
+        footer = [checkSum]
         msg.headerFields = header
         msg.footerFields = footer
 
@@ -44,53 +42,50 @@ class DummySession:
         msg.check_structure()
         msg.validate()
 
-        bl = msg.calc_body_length( mutate = True)
-        cs = msg.calc_check_sum( mutate = True )
+        bl = msg.calc_body_length(mutate=True)
+        cs = msg.calc_check_sum(mutate=True)
         msg.check_body_length()
-        bl2 = msg.calc_body_length( )
+        bl2 = msg.calc_body_length()
         cs2 = msg.calc_check_sum()
-        print bl, bl2, cs, cs2
+        print(bl, bl2, cs, cs2)
         self.msgSeqNum += 1
 
-
-
-from twisted.internet.protocol import Protocol, ClientFactory
 
 class Session(ClientFactory):
     def __init__(self,
-                 beginString, 
+                 beginString,
                  senderCompID,
-                 targetCompID ):
+                 targetCompID):
 
         self.sender = senderCompID
         self.target = targetCompID
-        
-        self.beginString  = fix.BeginString( beginString )
-        self.senderCompID = fix.SenderCompID( senderCompID )
-        self.targetCompID = fix.TargetCompID( targetCompID )
+
+        self.beginString = fix.BeginString(beginString)
+        self.senderCompID = fix.SenderCompID(senderCompID)
+        self.targetCompID = fix.TargetCompID(targetCompID)
         self.outMsgSeqNum = 1
-        self.inMsgSeqNum  = 1
-        
-        #self.bodyLength = pyfix.BodyLength(0)
-        #self.checkSum   = pyfix.CheckSum( 0 )
+        self.inMsgSeqNum = 1
+
+        # self.bodyLength = pyfix.BodyLength(0)
+        # self.checkSum   = pyfix.CheckSum( 0 )
 
     def setPersister(self, persister):
         self.persister = persister
 
     def compileMessage(self, msg):
-        sendingTime = fix.SendingTime( datetime.now() )
+        sendingTime = fix.SendingTime(datetime.now())
         bodyLength = fix.BodyLength(0)
-        checkSum   = fix.CheckSum(0)
+        checkSum = fix.CheckSum(0)
 
-        header = [ self.beginString,
-                   bodyLength,
-                   msg.msgTypeField,
-                   self.senderCompID,
-                   self.targetCompID,
-                   fix.MsgSeqNum(self.msgSeqNum),
-                   sendingTime]
+        header = [self.beginString,
+                  bodyLength,
+                  msg.msgTypeField,
+                  self.senderCompID,
+                  self.targetCompID,
+                  fix.MsgSeqNum(self.msgSeqNum),
+                  sendingTime]
 
-        footer = [ checkSum ]
+        footer = [checkSum]
         msg.headerFields = header
         msg.footerFields = footer
 
@@ -98,13 +93,10 @@ class Session(ClientFactory):
         msg.check_structure()
         msg.validate()
 
-        bl = msg.calc_body_length( mutate = True)
-        cs = msg.calc_check_sum( mutate = True )
+        bl = msg.calc_body_length(mutate=True)
+        cs = msg.calc_check_sum(mutate=True)
         msg.check_body_length()
-        bl2 = msg.calc_body_length( )
+        bl2 = msg.calc_body_length()
         cs2 = msg.calc_check_sum()
-        print bl, bl2, cs, cs2
+        print(bl, bl2, cs, cs2)
         self.msgSeqNum += 1
-
-
-
