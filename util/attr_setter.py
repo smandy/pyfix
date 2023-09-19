f6a222cde7e7d7c8ce94582a20b04bb798feef9b
Andy Smith <andyrsmith@gmail.com>
2023-09-19 14:28:53 +0100

    Fixup metaclasses.

--- util/attr_setter.py
+++ util/attr_setter.py
@@ -3,36 +3,37 @@
 def myCap(x):
     return x[0].upper() + x[1:]
 
+
 def attr_set_func(attr):
     def setter(self, x):
-        setattr( self, attr, x)
+        setattr(self, attr, x)
     return setter
 
-class attr_setter(type):
-    def __new__(_, name, bases, d):
-        if d.has_key('attrs'):
-            attrs = d.pop('attrs')
-            for attr in attrs:
-                methodName = "set%s" % myCap(attr)
-                d[methodName] = attr_set_func(attr)
-        return type( name, bases, d)
 
-if __name__=='__main__':
-    class A:
-        __metaclass__=attr_setter
-        attrs = [ 'orderManager','positionManager','clOrdIDGen']
+class attr_setter(type):
+    def __new__(mcs, name, bases, d):
+        print(f"{mcs} {name} {bases} {d} New has been called")
+        if 'attrs' in d:
+            attrs = d.pop('attrs')
+            d['__slots__'] = attrs
+            for attr in attrs:
+                methodName = f"set{myCap(attr)}"
+                d[methodName] = attr_set_func(attr)
+        return type(name, bases, d)
+
+
+if __name__ == '__main__' or True:
+    class A(metaclass=attr_setter):
+        __metaclass__ = attr_setter
+        attrs = ['orderManager', 'positionManager', 'clOrdIDGen']
+
         def normalMethod(self, x):
-            self.norm  = x
+            self.norm = x
 
         @classmethod
         def doCls(cls):
-            print "doCls %s" % cls 
-            pass
-        
+            print(f"doCls {cls}")
+
     a = A()
     a.setOrderManager(1)
     a.setPositionManager(32)
-    
-
-
-    
